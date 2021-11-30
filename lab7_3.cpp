874fc56656d0b52dc9dab9c69d71d16576b06983
Aumboon <unaweblanca@gmail.com>
2021-11-30 22:26:30 +0700

    lap7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 
 using namespace std;
+int adiff(int a, int b){
+   
+     int c = a - b;
+     if(c < 0){
+       c = -c;
+    }
+    
+    c = c % 360;
+    if (c > 180)
+        c = 360 - c;
+    return c;
+}
 
-
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
+int main() {
   cout << adiff(1000,280);
-  cout << adiff(60,244);
 }
